aa3d337800592f2b107824b8b9fbabec79d919ee
Rudro Samanta <rudro@uber.com>
2018-04-11 13:16:09 -0700

    Use Swift Package Manager to manage Dependencies
    - Updated README with build instructions
    - Deleted project and workspace files

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "Needle",
+    products: [
+        .executable(name: "needle", targets: ["needle"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.20.0"),
+    ],
+    targets: [
+        .target(
+            name: "needle",
+            dependencies: [
+                "SourceKittenFramework",
+            ],
+            path: "Needle"
+        )
+    ],
+    swiftLanguageVersions: [4]
+)
