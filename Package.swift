aba9b635134e80925230b4ba3e518dcd4b14a551
Rudro Samanta <rudro@uber.com>
2018-04-13 18:26:59 -0700

    Add Commander package for command-line parsing

--- Package.swift
+++ Package.swift
@@ -8,11 +8,13 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.20.0"),
+        .package(url: "https://github.com/kylef/Commander.git", from: "0.8.0"),
     ],
     targets: [
         .target(
             name: "needle",
             dependencies: [
+                "Commander",
                 "SourceKittenFramework",
             ]
         )
