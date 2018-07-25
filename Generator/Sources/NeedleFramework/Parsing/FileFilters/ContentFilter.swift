5d1dcae21387ea93a26499a0825379aab522c6da
Rudro Samanta <rudro@uber.com>
2018-07-24 21:20:14 -0700

    Minor uniformity update.

--- Generator/Sources/NeedleFramework/Parsing/FileFilters/ContentFilter.swift
+++ Generator/Sources/NeedleFramework/Parsing/FileFilters/ContentFilter.swift
@@ -36,11 +36,11 @@ class ContentFilter: FileFilter {
         }
 
         // Match actual component inheritance using Regex.
-        let containsComponentInheritance = (Regex(": *(" + needleModuleName + ".)?Component *<").firstMatch(in: content) != nil)
+        let containsComponentInheritance = (Regex(": *(\(needleModuleName).)?Component *<").firstMatch(in: content) != nil)
         if containsComponentInheritance {
             return true
         }
-        let containsDependencyInheritance = (Regex(": *(" + needleModuleName + ".)?Dependency").firstMatch(in: content) != nil)
+        let containsDependencyInheritance = (Regex(": *(\(needleModuleName).)?Dependency").firstMatch(in: content) != nil)
         if containsDependencyInheritance {
             return true
         }
