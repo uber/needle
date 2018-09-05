d4ef4e09d7989cb477dce38548c289ab25927b1d
Yi Wang <yiw@uber.com>
2018-09-05 16:54:00 -0700

    Add support for multiple source directories
    Some projects have multiple root source directories to parse, yet specifying the higher level ancestor directory would lead to too many noise files. This adds the support to specify multiple source directories to parse.

--- Generator/Sources/needle/GenerateCommand.swift
+++ Generator/Sources/needle/GenerateCommand.swift
@@ -29,8 +29,8 @@ class GenerateCommand: Command {
     let name = "generate"
 
     private let overview = "Generate DI code based on all Swift source files in a directory."
-    private let sourceRootPath: PositionalArgument<String>
     private let destinationPath: PositionalArgument<String>
+    private let sourceRootPaths: PositionalArgument<[String]>
     private let suffixes: OptionArgument<[String]>
     private let additionalImports: OptionArgument<[String]>
     private let scanPlugins: OptionArgument<Bool>
@@ -38,8 +38,8 @@ class GenerateCommand: Command {
 
     required init(parser: ArgumentParser) {
         let subparser = parser.add(subparser: name, overview: overview)
-        sourceRootPath = subparser.add(positional: "sourceRootPath", kind: String.self, usage: "Path to the root folder of Swift source files.", completion: .filename)
         destinationPath = subparser.add(positional: "destinationPath", kind: String.self, usage: "Path to the destination file of generated Swift DI code.", completion: .filename)
+        sourceRootPaths = subparser.add(positional: "sourceRootPaths", kind: [String].self, strategy: ArrayParsingStrategy.upToNextOption, usage: "Path to the root folder of Swift source files.", completion: .filename)
         suffixes = subparser.add(option: "--suffixes", shortName: "-sfx", kind: [String].self, usage: "Filename suffix(es) without extensions to exclude from parsing.", completion: .filename)
         scanPlugins = subparser.add(option: "--pluginized", shortName: "-p", kind: Bool.self, usage: "Whether or not to consider plugins when parsing.")
         additionalImports = subparser.add(option: "--additional-imports", shortName: "-ai", kind: [String].self, usage: "Additional modules to import in the generated file, in addition to the ones parsed from source files.", completion: .none)
@@ -47,22 +47,22 @@ class GenerateCommand: Command {
     }
 
     func execute(with arguments: ArgumentParser.Result) {
-        if let sourceRootPath = arguments.get(sourceRootPath) {
-            if let destinationPath = arguments.get(destinationPath) {
+        if let destinationPath = arguments.get(destinationPath) {
+            if let sourceRootPaths = arguments.get(sourceRootPaths) {
                 let suffixes = arguments.get(self.suffixes) ?? []
                 let additionalImports = arguments.get(self.additionalImports) ?? []
                 let scanPlugins = arguments.get(self.scanPlugins) ?? false
                 let headerDocPath = arguments.get(self.headerDocPath) ?? nil
                 if scanPlugins {
-                    PluginizedNeedle.generate(from: sourceRootPath, excludingFilesWith: suffixes, with: additionalImports, headerDocPath, to: destinationPath)
+                    PluginizedNeedle.generate(from: sourceRootPaths, excludingFilesWith: suffixes, with: additionalImports, headerDocPath, to: destinationPath)
                 } else {
-                    Needle.generate(from: sourceRootPath, excludingFilesWith: suffixes, with: additionalImports, headerDocPath, to: destinationPath)
+                    Needle.generate(from: sourceRootPaths, excludingFilesWith: suffixes, with: additionalImports, headerDocPath, to: destinationPath)
                 }
             } else {
-                fatalError("Missing destination path.")
+                fatalError("Missing source files root directories.")
             }
         } else {
-            fatalError("Missing source files root directory.")
+            fatalError("Missing destination path.")
         }
     }
 }
