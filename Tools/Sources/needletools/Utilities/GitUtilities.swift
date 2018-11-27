64b34098e50533220107d233705416f925d6e5fd
Yi Wang <neakor@gmail.com>
2018-11-27 15:31:38 -0800

    Initial implementation of needletools (#198)
    * Initial implementation of needletools
    
    This change adds the `deploy` command that deploys a new version of Needle to all the distribution channels. This change currently only archieves a new generator binary and pushes the new binary to the remote master branch. More functionality will be added for other distrubuction channels.
    
    * Remove Carthage setup from Tools
    
    * Make assert deploying more forgiving
    
    * Simplify version string formatting

---
+++ Tools/Sources/needletools/Utilities/GitUtilities.swift
@@ -0,0 +1,62 @@
+//
+//  Copyright (c) 2018. Uber Technologies
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+
+/// The errors produced by executing Git operations.
+enum GitErrors: Error {
+    /// The generic error with details in the associated value.
+    case generic(String)
+}
+
+/// Utility functions for invoking the GIT process.
+class GitUtilities {
+    
+    /// Retrieve the head commit SHA.
+    ///
+    /// - throws: `GitErrors.generic` if the current working directory
+    /// is not a git repository.
+    /// - returns: The head commit SHA.
+    static func headSHA() throws -> String {
+        let result = ProcessUtilities.execute(processName: "git", withArguments: ["rev-parse", "HEAD"])
+        if result.output.isEmpty {
+            throw GitErrors.generic(result.error)
+        } else {
+            return result.output
+        }
+    }
+    
+    /// Push a single file change to remote master branch.
+    ///
+    /// - parameter file: The single file to be committed and pushed.
+    /// - parameter version: The version number of the release.
+    static func push(file: String, withVersion version: String) throws {
+        var result = ProcessUtilities.execute(processName: "git", withArguments: ["add", file])
+        if !result.error.isEmpty {
+            throw GitErrors.generic(result.error)
+        }
+        
+        result = ProcessUtilities.execute(processName: "git", withArguments: ["commit", "-m", "'\(version)'"])
+        if !result.error.isEmpty {
+            throw GitErrors.generic(result.error)
+        }
+        
+        result = ProcessUtilities.execute(processName: "git", withArguments: ["push", "origin", "master"])
+        if !result.output.isEmpty {
+            throw GitErrors.generic(result.error)
+        }
+    }
+}
