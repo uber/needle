605ed28cf9ff0005486ce7c8733b75578e107b8a
Vivek Vichare <vichare@uber.com>
2019-12-09 13:59:13 -0800

    Update Generator dependency to a specific version of swift-common. We want to this in order to enable upgrade of the swift-common framework to Swift 5.1 without breaking Needle.

--- Generator/Package.swift
+++ Generator/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.23.1"),
         .package(url: "https://github.com/apple/swift-package-manager.git", .exact("0.3.0")),
         .package(url: "https://github.com/uber/swift-concurrency.git", .upToNextMajor(from: "0.6.5")),
-        .package(url: "https://github.com/uber/swift-common.git", .branch("master")),
+        .package(url: "https://github.com/uber/swift-common.git", .exact("0.1.0")),
     ],
     targets: [
         .target(
