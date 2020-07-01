e23be6985b06a5c31f5b566d4bd90cbac610a161
Rudro Samanta <rudro@uber.com>
2020-06-30 22:35:44 -0700

    Use newer version of swift-common as it contains a fix for the SHA256 utils

--- Generator/Package.swift
+++ Generator/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .package(url: "https://github.com/jpsim/SourceKitten.git", from: "0.23.1"),
         .package(url: "https://github.com/apple/swift-tools-support-core", .upToNextMajor(from: "0.1.5")),
         .package(url: "https://github.com/uber/swift-concurrency.git", .upToNextMajor(from: "0.6.5")),
-        .package(url: "https://github.com/uber/swift-common.git", .exact("0.2.0")),
+        .package(url: "https://github.com/uber/swift-common.git", .exact("0.3.0")),
     ],
     targets: [
         .target(
